80f5bc95b48bb6a08170ff4214f345e32752b4ab
canyangyang <2480992603@qq.com>
2020-10-21 11:25:30 +0800

    序列化dictionary

---
+++ Assets/Script/Bpath.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bpath 
+{
+    // Start is called before the first frame update
+    public string name;
+    public Dictionary<int, Vector3[]> dic = new Dictionary<int, Vector3[]>();
+
+}
---
+++ Assets/Script/BpathMgr.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+using Newtonsoft.Json;
+public class BpathMgr : MonoBehaviour
+{
+    public string dicname;
+    PathCreator creator;
+    [System.Serializable]
+    public struct bpath
+    {
+        public int number;
+        public GameObject path;
+    }
+    public bpath[] bpaths;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Bpath newbpath = new Bpath();
+        newbpath.name = dicname;
+        newbpath.dic = new Dictionary<int, Vector3[]>();
+        for(int i = 0; i < bpaths.Length; i++)
+        {
+            if (!newbpath.dic.ContainsKey(bpaths[i].number))
+            {
+                creator = bpaths[i].path.GetComponent<PathCreator>();
+                Debug.Log(creator.path.NumPoints);
+                Vector3[] points = creator.path.localPoints;
+                newbpath.dic.Add(bpaths[i].number, points);
+            }
+        }
+        string json = JsonConvert.SerializeObject(newbpath);
+        Debug.Log(json);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
