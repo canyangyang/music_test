4a4c4796342b162ae65c9f8f0edd24b7990cbdf1
canyangyang <2480992603@qq.com>
2020-10-21 13:47:55 +0800

    change to json

--- Assets/Script/BpathMgr.cs
+++ Assets/Script/BpathMgr.cs
@@ -12,31 +12,23 @@ public class BpathMgr : MonoBehaviour
     {
         public int number;
         public GameObject path;
-    }
-    public bpath[] bpaths;
-    public struct bpathdata
-    {
-        public int number;
         public Vector3[] points;
     }
-    public bpathdata[] bpathdatas;
+    public bpath[] bpaths;
+  
+ 
     // Start is called before the first frame update
     void Start()
     {
-        bpathdatas = new bpathdata[bpaths.Length];
+        
         //将结构体的数据复制给数据结构体
         for(int i = 0; i < bpaths.Length; i++)
         {
                 creator = bpaths[i].path.GetComponent<PathCreator>();
-                bpathdatas[i].number = bpaths[i].number;
+                bpaths[i].points = creator.path.localPoints;
                 Debug.Log(creator.path.NumPoints);
-               for(int j = 0; j < creator.path.NumPoints; j++)
-            {
-                Debug.Log(creator.path.GetPoint(j));
-                bpathdatas[i].points[j] = creator.path.GetPoint(j);
             }
-            }
-        string json =JsonUtility.ToJson(bpathdatas, true);
+        string json =JsonUtility.ToJson(bpaths, true);
         Debug.Log(json);
         }
         
